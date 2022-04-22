2e9caf9f955232e5537ebb77a1e80f3383693aa3
BruceRW3 <brwiggleston@gmail.com>
2022-04-22 14:36:32 -0400

    added Locations

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,27 @@ function App() {
   return (
     <div>
       <h1>Weather App</h1>
+      <div>
+        <label>
+          Add Location <input type="text" value="Irmo" />
+        </label>
+        <button>Search</button>
+      </div>
+
+      <div>
+        <h2>Locations</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>Knoxville</td></tr>
+            <tr><td>Greensboro</td></tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
