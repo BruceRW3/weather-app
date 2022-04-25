d4d4b923e54b272f8f2cf18cc687624af63b960e
BruceRW3 <brwiggleston@gmail.com>
2022-04-24 21:28:13 -0400

    finished weather app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import {FC, useState} from 'react';
 import './App.css';
+import {LocationSearch} from './components/LocationSearch';
+import {LocationTable} from './components/LocationTable';
+import { WeatherSummary } from './components/WeatherSummary';
+import { WeatherLocation } from './model/Weather';
+import { searchLocation } from './services/WeatherService';
+import {ErrorAlert, WarningAlert} from './components/Alerts'
 
-function App() {
+
+const App: FC = () => {
+  const [currentLocation, setCurrentLocation] = useState<WeatherLocation | null>(null);
+  const [locations, setLocations] = useState<WeatherLocation[]>([])
+  const [error, setError] = useState('');
+  const [warning, setWarning] = useState('');
+
+  const resetAlerts = () => {
+    setError('');
+    setWarning('');
+  }
+  
+  async function addLocation(term: string): Promise<void> {
+    resetAlerts();
+    const location = await searchLocation(term);
+
+    if (!location) {
+      setError(`No location found called '${term}'`);
+    } else if (locations.find(item => item.id === location.id)) {
+      setWarning(`Location '${term}' is already in the list.`);
+    } else {
+      setLocations([location, ...locations]);
+    }
+  }
+
   return (
-    <div>
+    <div className='container'>
       <h1>Weather App</h1>
-      <div>
-        <label>
-          Add Location <input type="text" value="Irmo" />
-        </label>
-        <button>Search</button>
-      </div>
+      
+      <LocationSearch onSearch={addLocation} />
+      <ErrorAlert message={error} />
+      <WarningAlert message={warning} />
+      <LocationTable locations={locations} 
+                      current={null} 
+                      onSelect={location => setCurrentLocation(location)}/>
 
-      <div>
-        <h2>Locations</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr><td>Knoxville</td></tr>
-            <tr><td>Greensboro</td></tr>
-          </tbody>
-        </table>
-      </div>
+      <WeatherSummary location={currentLocation}/>
     </div>
   );
 }
